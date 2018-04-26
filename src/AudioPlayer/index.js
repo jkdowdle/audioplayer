e1b8bdb4786e2893f43f320a2fcfd5a6cdbcf9a0
joshd <josh.dowdle@prioritydispatch.net>
2018-04-26 15:40:17 -0600

    Basics.

---
+++ src/AudioPlayer/index.js
@@ -0,0 +1,33 @@
+import React, { Component, createContext } from 'react'
+
+const { Consumer, Provider } = createContext()
+
+export default class AudioPlayer extends Component {
+  state = { playing: this.props.autoplay || false, source: this.props.source }
+
+  togglePlay = () => this.setState(({ playing }) => ({ playing: !playing }))
+
+  render = () => (
+    <Provider value={{ ...this.state, togglePlay: this.togglePlay }}>
+      {this.props.children}
+    </Provider>
+  )
+}
+
+export const Play = () => (
+<Consumer>
+  {({ playing, togglePlay }) => <button onClick={playing ? togglePlay : () => {}}>Play</button>}
+</Consumer>
+)
+
+export const Pause = () => (
+  <Consumer>
+    {({ playing, togglePlay }) => <button onClick={!playing ? togglePlay : () => {}}>Pause</button>}
+  </Consumer>
+)
+
+export const State = () => (
+  <Consumer>
+    {({ playing }) => <p>{playing ? 'playing' : 'paused'}</p>}
+  </Consumer>
+)
